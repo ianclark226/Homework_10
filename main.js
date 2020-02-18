27eaf30be0a976b48c7214542711aa102e38a3fb
Ian Clark <ianclark226@gmail.com>
2020-02-18 14:44:34 -0500

    2/18

--- main.js
+++ main.js
@@ -16,10 +16,71 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
-});
+    if (err) throw err;
+    runSearch();
+  });
+
+function runSearch() {
+    inquirer
+    .prompt({
+        message: "What would you like to do?",
+        choices: [
+            "View all Employees",
+            "View all Employees by department",
+            "View all Employees by Manager",
+            "Add employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+
+        ]
+    })
+    .then(function(answer) {
+        switch (answer.action) {
+            case "View all Employees":
+                employeeSearch();
+                break;
+
+                case  "View all Employees by department":
+                    departmentSearch();
+                    break;
+
+                    case "View all Employees by Manager":
+                        managerSearch();
+                        break;
+
+                        case "Add employee":
+                            addSearch();
+                            break;
+
+                            case "Remove Employee":
+                                removeSearch();
+                                break;
+
+                                case "Update Employee Role":
+                                    updateroleSearch();
+                                    break;
+
+                                    case "Update Employee Manager":
+                                        updatemanagerSearch();
+                                        break;
+
+
+
+
+
+        }
+    })
+}
+
+function addSearch() {
+    inquirer
+    .prompt({
+        message: "What is the Employees first name?"
+    })
+}
+
+
 
 function afterConnection() {
     connection.query("SELECT * FROM people", function(err, res) {
